264c5d3b57349f6dc31dbcd9f381b30d16d4df38
nguyenthanhtrungnhat <167590830+nguyenthanhtrungnhat@users.noreply.github.com>
2024-05-16 11:17:16 +0700

    git

---
+++ Lab4/Assignment1/Assignment1/Form1.Designer.cs
@@ -0,0 +1,375 @@
+﻿namespace Assignment1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.txtGmail = new System.Windows.Forms.TextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.btnChooseFile = new System.Windows.Forms.Button();
+            this.btnChose = new System.Windows.Forms.Button();
+            this.txtMess = new System.Windows.Forms.TextBox();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.txtFilePath = new System.Windows.Forms.TextBox();
+            this.txtMailTo = new System.Windows.Forms.TextBox();
+            this.txtMailFrom = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtPass);
+            this.groupBox1.Controls.Add(this.txtUserName);
+            this.groupBox1.Location = new System.Drawing.Point(92, 32);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Size = new System.Drawing.Size(272, 81);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Login Setting";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 56);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Password";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 23);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "UserName";
+            // 
+            // txtPass
+            // 
+            this.txtPass.Location = new System.Drawing.Point(85, 51);
+            this.txtPass.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.Size = new System.Drawing.Size(146, 20);
+            this.txtPass.TabIndex = 1;
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(85, 17);
+            this.txtUserName.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(146, 20);
+            this.txtUserName.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.txtPort);
+            this.groupBox2.Controls.Add(this.txtGmail);
+            this.groupBox2.Location = new System.Drawing.Point(368, 32);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox2.Size = new System.Drawing.Size(271, 81);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "SMTP Setting";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(44, 58);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Port";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(36, 23);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Gmail";
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(82, 54);
+            this.txtPort.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(119, 20);
+            this.txtPort.TabIndex = 1;
+            this.txtPort.Text = "587";
+            // 
+            // txtGmail
+            // 
+            this.txtGmail.Location = new System.Drawing.Point(82, 17);
+            this.txtGmail.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtGmail.Name = "txtGmail";
+            this.txtGmail.Size = new System.Drawing.Size(119, 20);
+            this.txtGmail.TabIndex = 0;
+            this.txtGmail.Text = "smtp.gmail.com";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.groupBox3.Controls.Add(this.label9);
+            this.groupBox3.Controls.Add(this.label8);
+            this.groupBox3.Controls.Add(this.label7);
+            this.groupBox3.Controls.Add(this.label6);
+            this.groupBox3.Controls.Add(this.label5);
+            this.groupBox3.Controls.Add(this.btnSend);
+            this.groupBox3.Controls.Add(this.btnChooseFile);
+            this.groupBox3.Controls.Add(this.btnChose);
+            this.groupBox3.Controls.Add(this.txtMess);
+            this.groupBox3.Controls.Add(this.txtTitle);
+            this.groupBox3.Controls.Add(this.txtFilePath);
+            this.groupBox3.Controls.Add(this.txtMailTo);
+            this.groupBox3.Controls.Add(this.txtMailFrom);
+            this.groupBox3.Location = new System.Drawing.Point(92, 134);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox3.Size = new System.Drawing.Size(548, 363);
+            this.groupBox3.TabIndex = 6;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Email Details";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(19, 214);
+            this.label9.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(50, 13);
+            this.label9.TabIndex = 12;
+            this.label9.Text = "Message";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(74, 173);
+            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(27, 13);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "Title";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(36, 129);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(69, 13);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "File Attached";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(53, 73);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(48, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Email To";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(47, 28);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Email From";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // btnSend
+            // 
+            this.btnSend.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnSend.Location = new System.Drawing.Point(404, 229);
+            this.btnSend.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(56, 19);
+            this.btnSend.TabIndex = 7;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = false;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // btnChooseFile
+            // 
+            this.btnChooseFile.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnChooseFile.Location = new System.Drawing.Point(404, 126);
+            this.btnChooseFile.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnChooseFile.Name = "btnChooseFile";
+            this.btnChooseFile.Size = new System.Drawing.Size(97, 19);
+            this.btnChooseFile.TabIndex = 6;
+            this.btnChooseFile.Text = "Choose File";
+            this.btnChooseFile.UseVisualStyleBackColor = false;
+            this.btnChooseFile.Click += new System.EventHandler(this.btnChooseFile_Click);
+            // 
+            // btnChose
+            // 
+            this.btnChose.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnChose.Location = new System.Drawing.Point(404, 70);
+            this.btnChose.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnChose.Name = "btnChose";
+            this.btnChose.Size = new System.Drawing.Size(97, 19);
+            this.btnChose.TabIndex = 5;
+            this.btnChose.Text = "Choose List";
+            this.btnChose.UseVisualStyleBackColor = false;
+            this.btnChose.Click += new System.EventHandler(this.btnChose_Click);
+            // 
+            // txtMess
+            // 
+            this.txtMess.Location = new System.Drawing.Point(85, 229);
+            this.txtMess.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtMess.Multiline = true;
+            this.txtMess.Name = "txtMess";
+            this.txtMess.Size = new System.Drawing.Size(278, 130);
+            this.txtMess.TabIndex = 4;
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Location = new System.Drawing.Point(109, 170);
+            this.txtTitle.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(254, 20);
+            this.txtTitle.TabIndex = 3;
+            // 
+            // txtFilePath
+            // 
+            this.txtFilePath.Location = new System.Drawing.Point(109, 125);
+            this.txtFilePath.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.Size = new System.Drawing.Size(254, 20);
+            this.txtFilePath.TabIndex = 2;
+            // 
+            // txtMailTo
+            // 
+            this.txtMailTo.Location = new System.Drawing.Point(109, 69);
+            this.txtMailTo.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtMailTo.Name = "txtMailTo";
+            this.txtMailTo.Size = new System.Drawing.Size(254, 20);
+            this.txtMailTo.TabIndex = 1;
+            // 
+            // txtMailFrom
+            // 
+            this.txtMailFrom.Location = new System.Drawing.Point(109, 25);
+            this.txtMailFrom.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtMailFrom.Name = "txtMailFrom";
+            this.txtMailFrom.Size = new System.Drawing.Size(254, 20);
+            this.txtMailFrom.TabIndex = 0;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(765, 545);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.TextBox txtGmail;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Button btnChooseFile;
+        private System.Windows.Forms.Button btnChose;
+        private System.Windows.Forms.TextBox txtMess;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.TextBox txtFilePath;
+        private System.Windows.Forms.TextBox txtMailTo;
+        private System.Windows.Forms.TextBox txtMailFrom;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Lab4/Assignment1/Assignment1/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            txtPass.PasswordChar = '*';
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void btnChose_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "txt file (*txt)|*.*";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                txtMailTo.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void btnChooseFile_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "txt file (*txt)|*.*";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                txtFilePath.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            var basicCredential = new NetworkCredential(txtUserName.Text, txtPass.Text);
+            SmtpClient client = new SmtpClient(txtGmail.Text, Int32.Parse(txtPort.Text));
+            client.UseDefaultCredentials = false;
+            client.EnableSsl = true;
+            client.Credentials = basicCredential;
+
+
+            MailAddress from = new MailAddress(txtMailFrom.Text);
+
+            string[] emailAddresses = File.ReadAllLines(txtMailTo.Text);
+
+            foreach (string email in emailAddresses)
+            {
+                if (!string.IsNullOrWhiteSpace(email))
+                {
+                    MailAddress to = new MailAddress(email);
+                    MailMessage message = new MailMessage(from, to);
+                    message.Body = txtMess.Text;
+                    message.BodyEncoding = System.Text.Encoding.UTF8;
+                    message.Subject = txtTitle.Text;
+                    message.SubjectEncoding = System.Text.Encoding.UTF8;
+                    if (!string.IsNullOrEmpty(txtFilePath.Text))
+                    {
+                        Attachment attachment = new Attachment(txtFilePath.Text);
+                        message.Attachments.Add(attachment);
+                    }
+                    client.Send(message);
+                }
+            }
+
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
