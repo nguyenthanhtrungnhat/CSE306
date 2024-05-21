d6fc6345015576e781b726293ae0cad12e1d3cc3
nguyenthanhtrungnhat <167590830+nguyenthanhtrungnhat@users.noreply.github.com>
2024-05-21 14:04:19 +0700

    git

---
+++ Demo/Demo2105/Demo2105/Program.cs
@@ -0,0 +1,6 @@
+﻿int workerThread;
+int portThread;
+
+ThreadPool.GetMaxThreads(out workerThread, out portThread);
+Console.WriteLine("\nMaximum worker threads: \t{0}" + "\nMaximum completion port threads: {1}", workerThread, portThread);
+Console.ReadKey();
